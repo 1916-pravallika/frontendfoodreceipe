58152cc80da7c7fae1b48eb41037bd765386cc99
SRIRAM PPRAVALLIKA <srirampravallika2006@gmail.com>
2025-09-23 11:27:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // allows access from external IPs
+    port: 8082,       // or whatever port your backend is running on
+    proxy: {
+      '/api': {
+        target: 'http://107.20.131.86:8082',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
