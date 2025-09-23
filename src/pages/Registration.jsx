b7884351e5eff662c69cf03549007452cbf856ba
SRIRAM PPRAVALLIKA <srirampravallika2006@gmail.com>
2025-09-23 09:49:08 +0530

    Update Registration.jsx

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -40,7 +40,7 @@ const Registration = () => {
 
     // console.log('Registration data:', formData);
 
-    axios.post('http://34.229.90.149:8082/register', formData).then((response) => {
+    axios.post('http://107.20.131.86:8082/register', formData).then((response) => {
       alert(response.data);
       navigate('/login');
     })
